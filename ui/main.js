9696e4545da433728da84a645c2cccb06cc45bac
Krish1212 <mailkimu@gmail.com>
2017-02-24 22:55:47 +0530

    local mods

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,17 @@
 console.log('Loaded!');
 
-var button = document.getElementById("counter");
-var span = document.getElementById("count");
-
-button.onclick = function(){
+var xbutton = document.getElementById("counter");
+var xspan = document.getElementById("count");
+var xcounter;
+xbutton.onclick = function(){
 	//create the request object
 	var request = new XMLHttpRequest();
 	//get the response
 	request.onreadystateChange = function(){
 		if(request.readyState === XMLHttpRequest.DONE){
 			if(request.status === 200){
-				var counter = request.responseText;
-				span.innerHTML = counter;
+				xcounter = request.responseText;
+				xspan.innerHTML = counter;
 			}
 		}
 	};
